67897c8594aecc985385cf2b469c27a7490d1a4d
ManuelDiGangi <157116580+ManuelDiGangi@users.noreply.github.com>
2024-07-17 17:51:26 +0200

    Add files via upload
    Aggiungere nuovo utente di Active Directory tramite powershell

---
+++ Microsoft/Active_Directory/AddUsersAd.ps1
@@ -0,0 +1,38 @@
+﻿Import-Module ActiveDirecotry
+
+$users = Import-Csv -Path "C:\path\to\the\file"
+
+foreach ($user in $users) {
+    $firstName = $user.firstName
+    $lastName = $user.lastName
+    $userName = $user.userName
+    $password = $user.password
+    $email = $user.email
+    $ou = $user.ou
+    $description = $user.description
+    $displayName = "$firstName $latName"
+
+    $existingUser = Get-ADUser -Filter {SamAccountName -eq $userName} -ErrorAction SilentlyContinue
+
+    if ($existingUser) {
+        Write-Host "L'utente $userName esiste gia"
+        #Set-ADUser potrei modificare le proprietà dell'utente
+    } else {
+        New_ADUser `
+            -GiveName $firstName `
+            -Surname $lastName `
+            -SamAccountName $userName `
+            -UserPrincipalName $email `
+            -Path $ou `
+            -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
+            -Enabled $true `
+            -DisplayName $displayName `
+            -Description $description `
+            -EmailAddress $email `
+            -PasswordNeverExpire $false `
+            -ChangePasswordAtLogon $true
+     }
+     Write-Host "Operazione completata per l'utente $userName."         
+}
+
+
